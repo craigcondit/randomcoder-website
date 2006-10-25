f4977288970c7597cdf22c5e9fbf0970db7511be
Craig Condit <ccondit@randomcoder.org>
2006-10-25 19:13:45 +0000

    Added warnings when deleting articles
    git-svn-id: https://randomcoder.org/repos/public/randomcoder-website/trunk@155 5bee6cb3-3d18-0410-8c93-a642edd49b48

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,9 @@
-/*
 Behaviour.register({
-	'.sectionHeading' : function(el) {
+	'A.delete' : function(el)
+	{
+		el.onclick = function()
+		{
+			return confirm("Are you sure you want to delete this article?\nThis action cannot be undone.");
+		};
 	}
 });
-*/
