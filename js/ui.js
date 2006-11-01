cc4476fd54a94fd15771d56bddf05d548f43989b
Craig Condit <ccondit@randomcoder.org>
2006-11-01 23:21:14 +0000

    Made login username box focused.
    git-svn-id: https://randomcoder.org/repos/public/randomcoder-website/trunk@184 5bee6cb3-3d18-0410-8c93-a642edd49b48

--- js/ui.js
+++ js/ui.js
@@ -1,9 +1,23 @@
+/*
+	Common behaviours for all pages on the site.
+*/
 Behaviour.register({
 	'A.delete' : function(el)
 	{
+		// add confirmation dialogs to all delete links
 		el.onclick = function()
 		{
 			return confirm("Are you sure you want to delete this article?\nThis action cannot be undone.");
 		};
+	},
+	'#j_username' : function(el)
+	{
+		// focus the username field of the login form, but only if there is only
+		// one form on the page
+		if (document.forms && document.forms.length == 1)
+		{
+			el.focus();
+			el.select();
+		}
 	}
 });
