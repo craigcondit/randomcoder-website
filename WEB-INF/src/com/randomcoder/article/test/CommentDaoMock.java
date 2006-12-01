1a586b91adfbe54bae940ce01f585081d3d84dfe
Craig Condit <ccondit@randomcoder.org>
2006-12-01 00:04:14 +0000

    Added CommentDao mock object.
    git-svn-id: https://randomcoder.org/repos/public/randomcoder-website/trunk@265 5bee6cb3-3d18-0410-8c93-a642edd49b48

---
+++ WEB-INF/src/com/randomcoder/article/test/CommentDaoMock.java
@@ -0,0 +1,63 @@
+package com.randomcoder.article.test;
+
+import java.util.*;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.randomcoder.article.*;
+
+public class CommentDaoMock implements CommentDao
+{
+	private final List<Comment> comments = new ArrayList<Comment>();
+	private long primaryKey;
+	
+	public Long create(Comment newInstance)
+	{
+		validateRequiredFields(newInstance);		
+		newInstance.setId(primaryKey++);
+		comments.add(newInstance);		
+		return newInstance.getId();
+	}
+
+	public void delete(Comment persistentObject)
+	{
+		Long id = persistentObject.getId();
+		
+		for (Iterator<Comment> it = comments.iterator(); it.hasNext();)
+		{
+			if (it.next().getId().equals(id))
+			{
+				it.remove();
+				return;
+			}
+		}
+		throw new IllegalArgumentException("NOT FOUND: id = " + id);
+	}
+
+	public Comment read(Long id)
+	{
+		for (Comment comment : comments)
+			if (comment.getId().equals(id))
+				return comment;
+		
+		return null;
+	}
+
+	private void validateRequiredFields(Comment comment)
+	{
+		if (comment.getArticle() == null)
+			throw new IllegalArgumentException("article required");
+		
+		if (comment.getContentType() == null)
+			throw new IllegalArgumentException("contentType required");
+		
+		if (comment.getCreationDate() == null)
+			throw new IllegalArgumentException("creationDate required");
+		
+		if (StringUtils.trimToNull(comment.getTitle()) == null)
+			throw new IllegalArgumentException("title required");
+		
+		if (StringUtils.trimToNull(comment.getContent()) == null)
+			throw new IllegalArgumentException("content required");
+	}
+}
