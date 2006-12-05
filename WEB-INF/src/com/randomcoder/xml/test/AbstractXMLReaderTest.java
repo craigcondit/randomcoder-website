2e44f2eaa6e0ac44345eae37637e6f80dcd452da
Craig Condit <ccondit@randomcoder.org>
2006-12-05 19:30:02 +0000

    Added test cases for AbstractXMLReader
    git-svn-id: https://randomcoder.org/repos/public/randomcoder-website/trunk@286 5bee6cb3-3d18-0410-8c93-a642edd49b48

---
+++ WEB-INF/src/com/randomcoder/xml/test/AbstractXMLReaderTest.java
@@ -0,0 +1,96 @@
+package com.randomcoder.xml.test;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.*;
+import org.xml.sax.*;
+import org.xml.sax.helpers.DefaultHandler;
+
+import com.randomcoder.xml.AbstractXMLReader;
+
+public class AbstractXMLReaderTest
+{
+	private AbstractXMLReaderMock reader;
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		reader = new AbstractXMLReaderMock();
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		reader = null;
+	}
+
+	@Test
+	public void testParse() throws SAXException, IOException
+	{
+		reader.parse("test/abstractxmlreader.xml");
+		assertEquals("test/abstractxmlreader.xml", reader.getInputSource().getSystemId());		
+	}
+
+	@Test
+	public void testGetContentHandler()
+	{
+		DefaultHandler handler = new DefaultHandler();
+		reader.setContentHandler(handler);
+		assertEquals(handler, reader.getContentHandler());
+	}
+
+	@Test
+	public void testGetDTDHandler()
+	{
+		DefaultHandler handler = new DefaultHandler();
+		reader.setDTDHandler(handler);
+		assertEquals(handler, reader.getDTDHandler());
+	}
+
+	@Test
+	public void testGetEntityResolver()
+	{
+		DefaultHandler handler = new DefaultHandler();
+		reader.setEntityResolver(handler);
+		assertEquals(handler, reader.getEntityResolver());
+	}
+
+	@Test
+	public void testGetErrorHandler()
+	{
+		DefaultHandler handler = new DefaultHandler();
+		reader.setErrorHandler(handler);
+		assertEquals(handler, reader.getErrorHandler());
+	}
+
+	@Test
+	public void testGetFeature() throws SAXException
+	{
+		reader.setFeature("test-feature", true);
+		assertTrue(reader.getFeature("test-feature"));
+		assertFalse(reader.getFeature("bogus-feature"));
+	}
+
+	@Test
+	public void testGetProperty() throws SAXException
+	{
+		reader.setProperty("test-property", "test-value");
+		assertEquals("test-value", reader.getProperty("test-property"));
+		assertNull(reader.getProperty("bogus-property"));
+	}
+
+	protected class AbstractXMLReaderMock extends AbstractXMLReader
+	{
+		private InputSource input;
+		
+		@Override
+		public void parse(InputSource _input) throws IOException, SAXException
+		{
+			input = _input;
+		}		
+		
+		public InputSource getInputSource() { return input; }
+	}
+}
