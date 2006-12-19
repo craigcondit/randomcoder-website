d4a8c56987028263ec3230faa3e7326cef2b69bb
Craig Condit <ccondit@randomcoder.org>
2006-12-19 19:05:15 +0000

    Added test cases for KeystoreCertificateFactoryBean
    git-svn-id: https://randomcoder.org/repos/public/randomcoder-website/trunk@311 5bee6cb3-3d18-0410-8c93-a642edd49b48

---
+++ WEB-INF/src/com/randomcoder/crypto/test/KeystoreCertificateFactoryBeanTest.java
@@ -0,0 +1,69 @@
+package com.randomcoder.crypto.test;
+
+import static org.junit.Assert.*;
+
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+import java.util.Properties;
+
+import org.junit.*;
+import org.springframework.core.io.*;
+
+import com.randomcoder.crypto.*;
+
+public class KeystoreCertificateFactoryBeanTest
+{
+	private static final String RES_XMLSEC_PROPS = "/xmlsec/xmlsec.properties";
+
+	private KeystoreCertificateFactoryBean target;
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		Properties properties = new Properties();
+		properties.load(getClass().getResourceAsStream(RES_XMLSEC_PROPS));
+
+		target = new KeystoreCertificateFactoryBean();
+		
+		Resource keystoreLocation = new ClassPathResource(properties.getProperty("keystore.resource"));
+		
+		target.setKeystoreLocation(keystoreLocation);
+		target.setKeystoreType(properties.getProperty("keystore.type"));
+		target.setKeystorePassword(properties.getProperty("keystore.password"));
+		target.setCertificateAlias(properties.getProperty("certificate.alias"));
+		target.setCertificatePassword(properties.getProperty("certificate.password"));
+		target.afterPropertiesSet();		
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{}
+
+	@Test
+	public void testGetObjectType()
+	{
+		assertEquals(CertificateContext.class, target.getObjectType());
+	}
+
+	@Test
+	public void testIsSingleton()
+	{
+		assertFalse(target.isSingleton());
+	}
+
+	@Test
+	public void testGetObject() throws Exception
+	{
+		Object obj = target.getObject();
+		assertNotNull(obj);
+		assertTrue(obj instanceof CertificateContext);
+		CertificateContext context = (CertificateContext) obj;
+		
+		X509Certificate cert = context.getCertificate();
+		assertNotNull(cert);
+		
+		PrivateKey key = context.getPrivateKey();
+		assertNotNull(key);
+	}
+
+}
