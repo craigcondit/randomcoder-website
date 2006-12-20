cce3c3cf9bec74095da7b0f49717a931df70fa1a
Craig Condit <ccondit@randomcoder.org>
2006-12-20 19:31:08 +0000

    Added NullLogoutHandler to workaround bug in acegi rememberme processing which doesn't handle null authentication object
    git-svn-id: https://randomcoder.org/repos/public/randomcoder-website/trunk@337 5bee6cb3-3d18-0410-8c93-a642edd49b48

---
+++ WEB-INF/src/com/randomcoder/security/NullLogoutHandler.java
@@ -0,0 +1,126 @@
+package com.randomcoder.security;
+
+import javax.servlet.http.*;
+
+import org.acegisecurity.*;
+import org.acegisecurity.ui.logout.LogoutHandler;
+import org.springframework.beans.factory.annotation.Required;
+
+/**
+ * LogoutHandler which forces an Authentication object to be present when
+ * none is specified.
+ * 
+ * <pre>
+ * Copyright (c) 2006, Craig Condit. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *     
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * </pre>
+ */
+public class NullLogoutHandler implements LogoutHandler
+{
+	private LogoutHandler logoutHandler;
+	private String username = "anonymous";
+	
+	/**
+	 * Sets the LogoutHandler to wrap.
+	 * @param logoutHandler logout handler
+	 */
+	@Required
+	public void setLogoutHandler(LogoutHandler logoutHandler)
+	{
+		this.logoutHandler = logoutHandler;
+	}
+	
+	/**
+	 * Sets the username to assign to the temporary authentication token.
+	 * <p>Defaults to <strong>anonymous</strong>.</p>
+	 * @param username user name
+	 */
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+	
+	/**
+	 * Logs out the current user.
+	 * <p>
+	 * If a null authentication object is provided, an anonymous token will be
+	 * supplied to the underlying logout handler.
+	 * </p>
+	 * @param request http request
+	 * @param response http response
+	 * @param authentication auth token
+	 */
+	public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+	{
+		if (authentication == null) authentication = new NullAuthentication(username);		
+		logoutHandler.logout(request, response, authentication);
+	}
+	
+	private static class NullAuthentication implements Authentication
+	{
+		private static final long serialVersionUID = -6144292174511841584L;
+		
+		private final String username;
+		
+		public NullAuthentication(String username) {
+			this.username = username;
+		}
+		
+		public GrantedAuthority[] getAuthorities()
+		{
+			return new GrantedAuthority[] {};
+		}
+
+		public Object getCredentials()
+		{
+			return null;
+		}
+
+		public Object getDetails()
+		{
+			return null;
+		}
+
+		public Object getPrincipal()
+		{
+			return username;
+		}
+
+		public boolean isAuthenticated()
+		{
+			return false;
+		}
+
+		public void setAuthenticated(boolean authenticated) throws IllegalArgumentException
+		{
+			if (authenticated)
+				throw new IllegalArgumentException("Cannot set authenticated to true");
+		}
+
+		public String getName()
+		{
+			return username;
+		}
+		
+	}
+}
---
+++ WEB-INF/src/com/randomcoder/security/test/NullLogoutHandlerTest.java
@@ -0,0 +1,118 @@
+package com.randomcoder.security.test;
+
+import static org.junit.Assert.*;
+
+import javax.servlet.http.*;
+
+import org.acegisecurity.*;
+import org.acegisecurity.ui.logout.LogoutHandler;
+import org.junit.*;
+import org.springframework.mock.web.*;
+
+import com.randomcoder.security.NullLogoutHandler;
+
+public class NullLogoutHandlerTest
+{
+	private NullLogoutHandler handler = null;
+	private LogoutHandlerMock mock = null;
+	private MockHttpServletRequest request = null;
+	private MockHttpServletResponse response = null;
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		mock = new LogoutHandlerMock();
+		handler = new NullLogoutHandler();
+		handler.setUsername("anonymous");
+		handler.setLogoutHandler(mock);
+		request = new MockHttpServletRequest();
+		response = new MockHttpServletResponse();
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		handler = null;
+		mock = null;
+		request = null;
+		response = null;
+	}
+
+	@Test
+	public void testLogoutNormal()
+	{
+		handler.logout(request, response, new AuthenticationMock());
+		Authentication auth = mock.getAuthentication();
+		assertNotNull(auth);
+		assertEquals(AuthenticationMock.class, auth.getClass());
+	}
+
+	@Test
+	public void testLogoutNull()
+	{
+		handler.logout(request, response, null);
+		Authentication auth = mock.getAuthentication();
+		assertNotNull(auth);
+	}
+	
+	@SuppressWarnings("unused")
+	private static class LogoutHandlerMock implements LogoutHandler
+	{		
+		private Authentication authentication = null;
+		
+		public LogoutHandlerMock() {}
+		
+		public void logout(HttpServletRequest request, HttpServletResponse response, Authentication auth)
+		{
+			authentication = auth;
+		}
+		
+		public Authentication getAuthentication()
+		{
+			return authentication;
+		}		
+	}
+	
+	@SuppressWarnings("unused")
+	private static class AuthenticationMock implements Authentication
+	{
+		private static final long serialVersionUID = 3105620828874678824L;
+
+		public AuthenticationMock() {}
+		
+		public GrantedAuthority[] getAuthorities()
+		{
+			return new GrantedAuthority[] {};
+		}
+
+		public Object getCredentials()
+		{
+			return null;
+		}
+
+		public Object getDetails()
+		{
+			return null;
+		}
+
+		public Object getPrincipal()
+		{
+			return "temp";
+		}
+
+		public boolean isAuthenticated()
+		{
+			return false;
+		}
+
+		public void setAuthenticated(boolean authenticated) throws IllegalArgumentException
+		{
+		}
+
+		public String getName()
+		{
+			return "temp";
+		}
+		
+	}
+}
