737aa044f33b8aead4453d38abb74e43d06aac09
Craig Condit <ccondit@randomcoder.org>
2006-12-21 00:47:38 +0000

    Added test cases for SamlException
    git-svn-id: https://randomcoder.org/repos/public/randomcoder-website/trunk@370 5bee6cb3-3d18-0410-8c93-a642edd49b48

---
+++ WEB-INF/src/com/randomcoder/saml/test/SamlExceptionTest.java
@@ -0,0 +1,80 @@
+package com.randomcoder.saml.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.randomcoder.saml.SamlException;
+
+public class SamlExceptionTest
+{
+	@Test
+	public void testSamlException()
+	{
+		try
+		{
+			throw new SamlException();
+		}
+		catch (SamlException e)
+		{
+			assertNull(e.getCause());
+			assertNull(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testSamlExceptionStringThrowable()
+	{
+		try
+		{
+			throw new Exception();				
+		}
+		catch (Exception cause)
+		{
+			try
+			{
+				throw new SamlException("message", cause);
+			}
+			catch (SamlException e)
+			{
+				assertNotNull(e.getCause());
+				assertEquals("message", e.getMessage());
+			}
+		}
+	}
+
+	@Test
+	public void testSamlExceptionString()
+	{
+		try
+		{
+			throw new SamlException("message");
+		}
+		catch (SamlException e)
+		{
+			assertNull(e.getCause());
+			assertEquals("message", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testSamlExceptionThrowable()
+	{
+		try
+		{
+			throw new Exception();				
+		}
+		catch (Exception cause)
+		{
+			try
+			{
+				throw new SamlException(cause);
+			}
+			catch (SamlException e)
+			{
+				assertNotNull(e.getCause());
+			}
+		}
+	}
+
+}
