41bc71ce49923d949fab71852e7b3f754dcb1e4e
Craig Condit <ccondit@randomcoder.org>
2006-12-21 21:42:49 +0000

    Extracted out factory metehod to get keystore certificate factory bean
    git-svn-id: https://randomcoder.org/repos/public/randomcoder-website/trunk@402 5bee6cb3-3d18-0410-8c93-a642edd49b48

--- WEB-INF/test/com/randomcoder/crypto/KeystoreCertificateFactoryBeanTest.java
+++ WEB-INF/test/com/randomcoder/crypto/KeystoreCertificateFactoryBeanTest.java
@@ -4,33 +4,19 @@ import static org.junit.Assert.*;
 
 import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
-import java.util.Properties;
 
 import org.junit.*;
-import org.springframework.core.io.*;
+
+import com.randomcoder.test.XmlSecurityFactory;
 
 public class KeystoreCertificateFactoryBeanTest
 {
-	private static final String RES_XMLSEC_PROPS = "/data/xml-security.properties";
-
 	private KeystoreCertificateFactoryBean target;
 	
 	@Before
 	public void setUp() throws Exception
 	{
-		Properties properties = new Properties();
-		properties.load(getClass().getResourceAsStream(RES_XMLSEC_PROPS));
-
-		target = new KeystoreCertificateFactoryBean();
-		
-		Resource keystoreLocation = new ClassPathResource(properties.getProperty("keystore.resource"));
-		
-		target.setKeystoreLocation(keystoreLocation);
-		target.setKeystoreType(properties.getProperty("keystore.type"));
-		target.setKeystorePassword(properties.getProperty("keystore.password"));
-		target.setCertificateAlias(properties.getProperty("certificate.alias"));
-		target.setCertificatePassword(properties.getProperty("certificate.password"));
-		target.afterPropertiesSet();		
+		target = XmlSecurityFactory.getKeystoreCertificateFactoryBean();
 	}
 
 	@After
