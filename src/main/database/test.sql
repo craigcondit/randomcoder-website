53bbd4a1ad22a52868b0b38acf8ced27b496a52b
Craig Condit <ccondit@randomcoder.org>
2007-01-15 23:15:17 +0000

    Fixed test scripts.
    git-svn-id: https://randomcoder.org/repos/public/randomcoder-website/trunk@601 5bee6cb3-3d18-0410-8c93-a642edd49b48

--- src/main/database/test.sql
+++ src/main/database/test.sql
@@ -3,29 +3,38 @@ INSERT INTO users (username, password, email, enabled) VALUES (
 	'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin@randomcoder.com', true
 );
 
+
+UPDATE roles SET name = 'ROLE_MANAGE_USERS' WHERE name = 'manage-users';
+UPDATE roles SET name = 'ROLE_POST_ARTICLES' WHERE name = 'article-post';
+UPDATE roles SET name = 'ROLE_MANAGE_ARTICLES' WHERE name = 'article-admin';
+UPDATE roles SET name = 'ROLE_DEVELOPMENT_DWR' WHERE name = 'development-dwr';
+UPDATE roles SET name = 'ROLE_MANAGE_TAGS' WHERE name = 'manage-tags';
+UPDATE roles SET name = 'ROLE_MANAGE_COMMENTS' WHERE name = 'manage-comments';
+
+
 INSERT INTO user_role_link (user_id, role_id) VALUES (
 	(SELECT user_id FROM users WHERE username = 'admin'),
-	(SELECT role_id FROM roles WHERE name = 'manage-users')
+	(SELECT role_id FROM roles WHERE name = 'ROLE_MANAGE_USERS')
 );
 
 INSERT INTO user_role_link (user_id, role_id) VALUES (
 	(SELECT user_id FROM users WHERE username = 'admin'),
-	(SELECT role_id FROM roles WHERE name = 'article-admin')
+	(SELECT role_id FROM roles WHERE name = 'ROLE_MANAGE_ARTICLES')
 );
 
 INSERT INTO user_role_link (user_id, role_id) VALUES (
 	(SELECT user_id FROM users WHERE username = 'admin'),
-	(SELECT role_id FROM roles WHERE name = 'manage-tags')
+	(SELECT role_id FROM roles WHERE name = 'ROLE_MANAGE_TAGS')
 );
 
 INSERT INTO user_role_link (user_id, role_id) VALUES (
 	(SELECT user_id FROM users WHERE username = 'admin'),
-	(SELECT role_id FROM roles WHERE name = 'manage-comments')
+	(SELECT role_id FROM roles WHERE name = 'ROLE_MANAGE_COMMENTS')
 );
 
 INSERT INTO user_role_link (user_id, role_id) VALUES (
 	(SELECT user_id FROM users WHERE username = 'admin'),
-	(SELECT role_id FROM roles WHERE name = 'development-dwr')
+	(SELECT role_id FROM roles WHERE name = 'ROLE_DEVELOPMENT_DWR')
 );
 
 -- Add test user, password 'test'
@@ -35,5 +44,5 @@ INSERT INTO users (username, password, email, enabled) VALUES (
 
 INSERT INTO user_role_link (user_id, role_id) VALUES (
 	(SELECT user_id FROM users WHERE username = 'test'),
-	(SELECT role_id FROM roles WHERE name = 'article-post')
+	(SELECT role_id FROM roles WHERE name = 'ROLE_POST_ARTICLES')
 );
