04a5799ea3d67ab34381363d0b525fb8fedc01f9
Craig Condit <ccondit@randomcoder.org>
2013-06-07 17:40:17 -0700

    Fixed JSTL template view test

--- src/test/java/org/randomcoder/springmvc/JstlTemplateViewTest.java
+++ src/test/java/org/randomcoder/springmvc/JstlTemplateViewTest.java
@@ -1,26 +1,42 @@
 package org.randomcoder.springmvc;
 
+import static org.easymock.EasyMock.*;
+
 import java.util.*;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 import junit.framework.TestCase;
 
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.mock.web.*;
-
+import org.easymock.*;
 import org.randomcoder.test.mock.springmvc.JstlTemplateViewMock;
+import org.springframework.web.context.WebApplicationContext;
 
 @SuppressWarnings("javadoc")
 public class JstlTemplateViewTest extends TestCase
 {
 	private JstlTemplateViewMock view;
 	private JstlTemplateViewMock parent;
+	private IMocksControl control;
+	private HttpServletRequest req;
+	private WebApplicationContext wac;
+	private ServletContext sc;
 	
 	@Override
 	public void setUp() throws Exception
 	{
+		control = createControl();
+		req = control.createMock(HttpServletRequest.class);
+		sc = control.createMock(ServletContext.class);
+		wac = control.createMock(WebApplicationContext.class);
+		
 		parent = new JstlTemplateViewMock();
 		view = new JstlTemplateViewMock();
 		view.setParent(parent);
+		
+		view.setServletContext(sc);
+		view.setApplicationContext(wac);
 	}
 
 	@Override
@@ -28,6 +44,10 @@ public class JstlTemplateViewTest extends TestCase
 	{
 		view = null;
 		parent = null;
+		wac = null;
+		sc = null;
+		req = null;
+		control = null;
 	}
 
 	public void testGetUrl()
@@ -52,6 +72,7 @@ public class JstlTemplateViewTest extends TestCase
 		assertEquals("parent", view.getTemplateName());
 	}
 
+	@SuppressWarnings("unchecked")
 	public void testExposeHelpers() throws Exception
 	{
 		Map<String, Object> parentMap = new HashMap<String, Object>();
@@ -65,15 +86,31 @@ public class JstlTemplateViewTest extends TestCase
 		
 		parent.setAttributes(parentMap);
 		view.setAttributes(childMap);
+
+		Capture<Object> cm = new Capture<Object>();
+
+		control.reset();
 		
-		// set some necessary container properties
-		view.setServletContext(new MockServletContext());
-		view.setApplicationContext(new GenericApplicationContext());
+		expect(wac.getServletContext()).andStubReturn(null);
+		expect(wac.getBean(isA(String.class), isA(Class.class))).andStubReturn(null);
 		
-		MockHttpServletRequest request = new MockHttpServletRequest();
-		view.exposeHelpers(request);
+		req.setAttribute(eq("template"), capture(cm));
 		
-		Map templateMap = (Map) request.getAttribute("template");
+		expect(req.getAttribute("org.springframework.web.servlet.support.RequestContext.CONTEXT")).andStubReturn(wac);
+		expect(req.getAttribute(isA(String.class))).andStubReturn(null);
+		expect(req.getSession(false)).andStubReturn(null);
+		expect(req.getLocale()).andStubReturn(null);
+		req.setAttribute(isA(String.class), anyObject());
+		expectLastCall().anyTimes();
+		
+		expect(sc.getAttribute(isA(String.class))).andStubReturn(null);
+		
+		control.replay();
+		
+		view.exposeHelpers(req);
+		control.verify();
+		
+		Map templateMap = (Map) cm.getValue();
 		
 		assertEquals("parent", templateMap.get("parent"));
 		assertEquals("child", templateMap.get("child"));
