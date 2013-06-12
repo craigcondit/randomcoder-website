a7a9a419e706b1fdb6197e0aff452c8e7285ddc4
Craig Condit <ccondit@randomcoder.org>
2013-06-12 08:03:53 -0700

    Removed embedded license from javadoc; formatting; fixed warnings

--- src/main/java/org/randomcoder/feed/AtomFeedGenerator.java
+++ src/main/java/org/randomcoder/feed/AtomFeedGenerator.java
@@ -25,31 +25,6 @@ import org.xml.sax.SAXException;
 
 /**
  * Generator for Atom 1.0 feeds.
- * 
- * <pre>
- * Copyright (c) 2007, Craig Condit. All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright notice,
- *     this list of conditions and the following disclaimer in the documentation
- *     and/or other materials provided with the distribution.
- *     
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- * </pre>
  */
 public class AtomFeedGenerator implements FeedGenerator
 {
@@ -58,26 +33,26 @@ public class AtomFeedGenerator implements FeedGenerator
 	private static final String THREAD_NS_PREFIX = "thr";
 	private static final String XHTML_NS = "http://www.w3.org/1999/xhtml";
 	private static final String XHTML_NS_PREFIX = "xhtml";
-	
+
 	private AppInfoBusiness appInfoBusiness;
 	private URL baseUrl;
 	private String uriPrefix;
 	private ContentFilter contentFilter;
-	
+
 	/**
 	 * Sets the base URL to use for articles.
 	 * 
 	 * @param baseUrl
-	 *            base url
+	 *          base URL
 	 * @throws MalformedURLException
-	 *             if URL is invalid
+	 *           if URL is invalid
 	 */
 	@Required
 	public void setBaseUrl(String baseUrl) throws MalformedURLException
 	{
 		this.baseUrl = new URL(baseUrl);
 	}
-	
+
 	/**
 	 * Sets the URI prefix used for generating unique identifiers.
 	 * 
@@ -89,7 +64,7 @@ public class AtomFeedGenerator implements FeedGenerator
 	{
 		this.uriPrefix = uriPrefix;
 	}
-	
+
 	/**
 	 * Sets the content filter to use for transforming articles into XHTML.
 	 * 
@@ -101,7 +76,7 @@ public class AtomFeedGenerator implements FeedGenerator
 	{
 		this.contentFilter = contentFilter;
 	}
-	
+
 	/**
 	 * Sets the AppInfoBusiness instance to use.
 	 * 
@@ -113,7 +88,7 @@ public class AtomFeedGenerator implements FeedGenerator
 	{
 		this.appInfoBusiness = appInfoBusiness;
 	}
-	
+
 	@Override
 	public String getContentType()
 	{
@@ -126,9 +101,9 @@ public class AtomFeedGenerator implements FeedGenerator
 		// need to write out XML
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		dbf.setNamespaceAware(true);
-		
+
 		DecimalFormat df = new DecimalFormat("####################");
-		
+
 		DocumentBuilder db = null;
 		try
 		{
@@ -138,19 +113,19 @@ public class AtomFeedGenerator implements FeedGenerator
 		{
 			throw new FeedConfigurationException("Unable to generate document builder", e);
 		}
-		
+
 		Document doc = db.newDocument();
 		Element root = doc.createElementNS(ATOM_1_0_NS, "feed");
 		root.setAttributeNS(XMLNS_ATTRIBUTE_NS_URI, "xmlns:" + THREAD_NS_PREFIX, THREAD_NS);
 		root.setAttributeNS(XMLNS_ATTRIBUTE_NS_URI, "xmlns:" + XHTML_NS_PREFIX, XHTML_NS);
-		
+
 		doc.appendChild(root);
 
 		// write feed title
 		Element titleEl = doc.createElementNS(ATOM_1_0_NS, "title");
 		titleEl.appendChild(doc.createTextNode(info.getTitle()));
 		root.appendChild(titleEl);
-		
+
 		// write feed subtitle
 		String subtitle = info.getSubtitle();
 		if (subtitle != null)
@@ -159,21 +134,21 @@ public class AtomFeedGenerator implements FeedGenerator
 			subtitleEl.appendChild(doc.createTextNode(subtitle));
 			root.appendChild(subtitleEl);
 		}
-		
+
 		// write feed generator
 		Element generatorEl = doc.createElementNS(ATOM_1_0_NS, "generator");
 		generatorEl.setAttribute("uri", "https://randomcoder.org/");
 		generatorEl.setAttribute("version", appInfoBusiness.getApplicationVersion());
 		generatorEl.appendChild(doc.createTextNode(appInfoBusiness.getApplicationName()));
 		root.appendChild(generatorEl);
-		
+
 		// write feed URL
 		Element feedEl = doc.createElementNS(ATOM_1_0_NS, "link");
 		feedEl.setAttribute("rel", "self");
 		feedEl.setAttribute("type", "application/atom+xml");
 		feedEl.setAttribute("href", info.getFeedUrl().toExternalForm());
 		root.appendChild(feedEl);
-		
+
 		// write alternate URL
 		URL altUrl = info.getAltUrl();
 		if (altUrl != null)
@@ -184,27 +159,27 @@ public class AtomFeedGenerator implements FeedGenerator
 			altEl.setAttribute("href", altUrl.toExternalForm());
 			root.appendChild(altEl);
 		}
-		
+
 		// write feed id
 		Element idEl = doc.createElementNS(ATOM_1_0_NS, "id");
 		idEl.appendChild(doc.createTextNode(uriPrefix + info.getFeedId()));
 		root.appendChild(idEl);
-		
+
 		// write feed updated date
 		Element updatedEl = doc.createElementNS(ATOM_1_0_NS, "updated");
-		root.appendChild(updatedEl);		
-		
+		root.appendChild(updatedEl);
+
 		Date feedUpdated = null;
-		
+
 		for (Article article : info.getArticles())
 		{
 			Element entryEl = doc.createElementNS(ATOM_1_0_NS, "entry");
-			
+
 			// write title
 			Element articleTitleEl = doc.createElementNS(ATOM_1_0_NS, "title");
-			articleTitleEl.appendChild(doc.createTextNode(article.getTitle()));			
+			articleTitleEl.appendChild(doc.createTextNode(article.getTitle()));
 			entryEl.appendChild(articleTitleEl);
-			
+
 			URL articleUrl = null;
 			try
 			{
@@ -214,14 +189,14 @@ public class AtomFeedGenerator implements FeedGenerator
 			{
 				throw new FeedException("Unable to generate feed URL", e);
 			}
-			
+
 			// write article link
 			Element articleAltEl = doc.createElementNS(ATOM_1_0_NS, "link");
 			articleAltEl.setAttribute("rel", "alternate");
 			articleAltEl.setAttribute("type", "text/html");
 			articleAltEl.setAttribute("href", articleUrl.toExternalForm());
 			entryEl.appendChild(articleAltEl);
-			
+
 			URL replyUrl = null;
 			try
 			{
@@ -231,7 +206,7 @@ public class AtomFeedGenerator implements FeedGenerator
 			{
 				throw new FeedException("Unable to generate reply URL", e);
 			}
-			
+
 			// write article reply link
 			Element repliesEl = doc.createElementNS(ATOM_1_0_NS, "link");
 			repliesEl.setAttribute("rel", "replies");
@@ -239,21 +214,21 @@ public class AtomFeedGenerator implements FeedGenerator
 			repliesEl.setAttribute("href", replyUrl.toExternalForm());
 			repliesEl.setAttributeNS(THREAD_NS, THREAD_NS_PREFIX + ":count", Integer.toString(article.getComments().size()));
 			entryEl.appendChild(repliesEl);
-			
+
 			// write id
 			Element articleIdEl = doc.createElementNS(ATOM_1_0_NS, "id");
 			articleIdEl.appendChild(doc.createTextNode(uriPrefix + "article-" + df.format(article.getId())));
 			entryEl.appendChild(articleIdEl);
-			
+
 			// write published
 			Date published = article.getCreationDate();
 			Element articlePublishedEl = doc.createElementNS(ATOM_1_0_NS, "published");
 			articlePublishedEl.appendChild(doc.createTextNode(formatDate(published)));
 			entryEl.appendChild(articlePublishedEl);
-			
+
 			if (feedUpdated == null || feedUpdated.before(published))
 				feedUpdated = published;
-			
+
 			// write modified
 			Date updated = article.getModificationDate();
 			if (updated != null)
@@ -261,14 +236,14 @@ public class AtomFeedGenerator implements FeedGenerator
 				Element articleUpdatedEl = doc.createElementNS(ATOM_1_0_NS, "updated");
 				articleUpdatedEl.appendChild(doc.createTextNode(formatDate(updated)));
 				entryEl.appendChild(articleUpdatedEl);
-				
+
 				if (feedUpdated == null || feedUpdated.before(updated))
 					feedUpdated = updated;
 			}
-			
+
 			// write author
-			String authorName = null;
-			String authorUrl = null;
+			String authorName;
+			String authorUrl;
 			User createdBy = article.getCreatedByUser();
 			if (createdBy == null)
 			{
@@ -280,33 +255,33 @@ public class AtomFeedGenerator implements FeedGenerator
 				authorName = createdBy.getUserName();
 				authorUrl = createdBy.getWebsite();
 			}
-			
+
 			Element authorEl = doc.createElementNS(ATOM_1_0_NS, "author");
-			
+
 			Element authorNameEl = doc.createElementNS(ATOM_1_0_NS, "name");
 			authorNameEl.appendChild(doc.createTextNode(authorName));
 			authorEl.appendChild(authorNameEl);
-						
+
 			if (DataValidationUtils.isValidUrl(authorUrl))
 			{
 				Element authorUriEl = doc.createElementNS(ATOM_1_0_NS, "uri");
 				authorUriEl.appendChild(doc.createTextNode(authorUrl));
 				authorEl.appendChild(authorUriEl);
 			}
-			
+
 			entryEl.appendChild(authorEl);
-			
+
 			// write categories
 			for (Tag tag : article.getTags())
 			{
 				Element categoryEl = doc.createElementNS(ATOM_1_0_NS, "category");
 				categoryEl.setAttribute("term", tag.getName());
-				categoryEl.setAttribute("label", tag.getDisplayName());				
+				categoryEl.setAttribute("label", tag.getDisplayName());
 				entryEl.appendChild(categoryEl);
 			}
 
 			String summary = article.getSummary();
-			
+
 			if (summary != null)
 			{
 				// write summary -- will need content filter
@@ -314,7 +289,7 @@ public class AtomFeedGenerator implements FeedGenerator
 				summaryEl.setAttribute("type", "xhtml");
 				summaryEl.setAttributeNS(javax.xml.XMLConstants.XML_NS_URI, "xml:lang", "en-US");
 				summaryEl.setAttributeNS(javax.xml.XMLConstants.XML_NS_URI, "xml:base", articleUrl.toExternalForm());
-				
+
 				try
 				{
 					addXHTML(doc, summaryEl, summary, article.getContentType());
@@ -323,16 +298,16 @@ public class AtomFeedGenerator implements FeedGenerator
 				{
 					throw new FeedException("Unable to generate summary for article with id " + df.format(article.getId()), e);
 				}
-				
+
 				entryEl.appendChild(summaryEl);
 			}
-						
+
 			// write content
 			Element contentEl = doc.createElementNS(ATOM_1_0_NS, "content");
 			contentEl.setAttribute("type", "xhtml");
 			contentEl.setAttributeNS(javax.xml.XMLConstants.XML_NS_URI, "xml:lang", "en-US");
 			contentEl.setAttributeNS(javax.xml.XMLConstants.XML_NS_URI, "xml:base", articleUrl.toExternalForm());
-			
+
 			try
 			{
 				addXHTML(doc, contentEl, article.getContent(), article.getContentType());
@@ -343,16 +318,16 @@ public class AtomFeedGenerator implements FeedGenerator
 			}
 
 			entryEl.appendChild(contentEl);
-			
+
 			root.appendChild(entryEl);
 		}
-		
+
 		if (feedUpdated == null)
 			feedUpdated = new Date();
-		
+
 		updatedEl.appendChild(doc.createTextNode(formatDate(feedUpdated)));
-		
-		StringWriter writer = new StringWriter();		
+
+		StringWriter writer = new StringWriter();
 		try
 		{
 			XmlUtils.prettyPrint(doc, new StreamResult(writer));
@@ -361,16 +336,15 @@ public class AtomFeedGenerator implements FeedGenerator
 		{
 			throw new FeedException("Unable to generate XML for feed", e);
 		}
-		
+
 		return writer.toString();
 	}
-	
-	private void addXHTML(Document doc, Element parent, String text, ContentType contentType)
-	throws TransformerException, SAXException, IOException
+
+	private void addXHTML(Document doc, Element parent, String text, ContentType contentType) throws TransformerException, SAXException, IOException
 	{
 		Element root = doc.createElementNS(XHTML_NS, "div");
 		Element tempRoot = doc.createElementNS(XHTML_NS, "div");
-		
+
 		ContentUtils.formatText(text, null, contentType, contentFilter, new DOMResult(tempRoot));
 
 		// copy children to remove extra nesting
@@ -382,14 +356,14 @@ public class AtomFeedGenerator implements FeedGenerator
 			child.removeChild(node);
 			root.appendChild(node);
 		}
-		
+
 		parent.appendChild(root);
 	}
-	
+
 	private String formatDate(Date date)
 	{
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
 		String text = sdf.format(date);
-		return  text.substring(0, text.length() - 2) + ":" + text.substring(text.length() - 2);
+		return text.substring(0, text.length() - 2) + ":" + text.substring(text.length() - 2);
 	}
 }
--- src/main/java/org/randomcoder/feed/Rss20FeedGenerator.java
+++ src/main/java/org/randomcoder/feed/Rss20FeedGenerator.java
@@ -20,38 +20,13 @@ import org.xml.sax.SAXException;
 
 /**
  * Generator for RSS 1.0 feeds.
- * 
- * <pre>
- * Copyright (c) 2007, Craig Condit. All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright notice,
- *     this list of conditions and the following disclaimer in the documentation
- *     and/or other materials provided with the distribution.
- *     
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- * </pre>
  */
 public class Rss20FeedGenerator implements FeedGenerator
 {
 	private AppInfoBusiness appInfoBusiness;
 	private URL baseUrl;
 	private ContentFilter contentFilter;
-	
+
 	/**
 	 * Sets the application information for this feed.
 	 * 
@@ -63,13 +38,14 @@ public class Rss20FeedGenerator implements FeedGenerator
 	{
 		this.appInfoBusiness = appInfoBusiness;
 	}
-	
+
 	/**
 	 * Sets the base URL to use for articles.
 	 * 
 	 * @param baseUrl
-	 *            base url
-	 * @throws MalformedURLException if URL is invalid
+	 *          base URL
+	 * @throws MalformedURLException
+	 *           if URL is invalid
 	 */
 	@Required
 	public void setBaseUrl(String baseUrl) throws MalformedURLException
@@ -95,9 +71,9 @@ public class Rss20FeedGenerator implements FeedGenerator
 		// need to write out XML
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		dbf.setNamespaceAware(true);
-		
+
 		DecimalFormat df = new DecimalFormat("####################");
-		
+
 		DocumentBuilder db = null;
 		try
 		{
@@ -107,22 +83,22 @@ public class Rss20FeedGenerator implements FeedGenerator
 		{
 			throw new FeedConfigurationException("Unable to generate document builder", e);
 		}
-		
+
 		Document doc = db.newDocument();
 		Element root = doc.createElement("rss");
 		root.setAttribute("version", "2.0");
 		doc.appendChild(root);
-		
+
 		Element channelEl = doc.createElement("channel");
 
 		Element titleEl = doc.createElement("title");
 		titleEl.appendChild(doc.createTextNode(info.getTitle()));
 		channelEl.appendChild(titleEl);
-		
+
 		Element linkEl = doc.createElement("link");
 		linkEl.appendChild(doc.createTextNode(info.getAltUrl().toExternalForm()));
 		channelEl.appendChild(linkEl);
-		
+
 		String subtitle = info.getSubtitle();
 		if (subtitle != null)
 		{
@@ -130,11 +106,11 @@ public class Rss20FeedGenerator implements FeedGenerator
 			descriptionEl.appendChild(doc.createTextNode(subtitle));
 			channelEl.appendChild(descriptionEl);
 		}
-		
+
 		Element docsEl = doc.createElement("docs");
 		docsEl.appendChild(doc.createTextNode("http://blogs.law.harvard.edu/tech/rss"));
 		channelEl.appendChild(docsEl);
-		
+
 		Element generatorEl = doc.createElement("generator");
 		generatorEl.appendChild(doc.createTextNode(appInfoBusiness.getApplicationName() + " " + appInfoBusiness.getApplicationVersion()));
 		channelEl.appendChild(generatorEl);
@@ -143,20 +119,20 @@ public class Rss20FeedGenerator implements FeedGenerator
 		languageEl.appendChild(doc.createTextNode("en-us"));
 		channelEl.appendChild(languageEl);
 
-		Element pubDateEl = doc.createElement("pubDate");		
+		Element pubDateEl = doc.createElement("pubDate");
 		channelEl.appendChild(pubDateEl);
 
 		Date feedUpdated = null;
-		
+
 		for (Article article : info.getArticles())
 		{
 			Element itemEl = doc.createElement("item");
-			
+
 			// write title
 			Element articleTitleEl = doc.createElement("title");
-			articleTitleEl.appendChild(doc.createTextNode(article.getTitle()));			
+			articleTitleEl.appendChild(doc.createTextNode(article.getTitle()));
 			itemEl.appendChild(articleTitleEl);
-			
+
 			URL articleUrl = null;
 			try
 			{
@@ -171,24 +147,24 @@ public class Rss20FeedGenerator implements FeedGenerator
 			Element articleLinkEl = doc.createElement("link");
 			articleLinkEl.appendChild(doc.createTextNode(articleUrl.toExternalForm()));
 			itemEl.appendChild(articleLinkEl);
-			
+
 			// write guid
 			Element articleGuidEl = doc.createElement("guid");
 			if (article.getPermalink() != null)
 				articleGuidEl.setAttribute("isPermaLink", "true");
-			
+
 			articleGuidEl.appendChild(doc.createTextNode(articleUrl.toExternalForm()));
 			itemEl.appendChild(articleGuidEl);
-			
+
 			// write published
 			Date published = article.getCreationDate();
 			Element articlePubDateEl = doc.createElement("pubDate");
 			articlePubDateEl.appendChild(doc.createTextNode(formatDate(published)));
 			itemEl.appendChild(articlePubDateEl);
-			
+
 			if (feedUpdated == null || feedUpdated.before(published))
 				feedUpdated = published;
-			
+
 			// write categories
 			for (Tag tag : article.getTags())
 			{
@@ -199,11 +175,11 @@ public class Rss20FeedGenerator implements FeedGenerator
 
 			// write content
 			Element descriptionEl = doc.createElement("description");
-			
+
 			String content = article.getSummary();
 			if (content == null)
 				content = article.getContent();
-			
+
 			try
 			{
 				addXHTML(doc, descriptionEl, content, article.getContentType(), articleUrl);
@@ -212,20 +188,20 @@ public class Rss20FeedGenerator implements FeedGenerator
 			{
 				throw new FeedException("Unable to generate description for article with id " + df.format(article.getId()), e);
 			}
-			
+
 			itemEl.appendChild(descriptionEl);
-						
+
 			channelEl.appendChild(itemEl);
 		}
-				
+
 		if (feedUpdated == null)
 			feedUpdated = new Date();
 
 		pubDateEl.appendChild(doc.createTextNode(formatDate(feedUpdated)));
 
 		root.appendChild(channelEl);
-		
-		StringWriter writer = new StringWriter();		
+
+		StringWriter writer = new StringWriter();
 		try
 		{
 			XmlUtils.prettyPrint(doc, new StreamResult(writer));
@@ -234,8 +210,8 @@ public class Rss20FeedGenerator implements FeedGenerator
 		{
 			throw new FeedException("Unable to generate XML for feed", e);
 		}
-		
-		return writer.toString();	
+
+		return writer.toString();
 	}
 
 	@Override
@@ -243,16 +219,16 @@ public class Rss20FeedGenerator implements FeedGenerator
 	{
 		return "application/rss+xml";
 	}
-	
+
 	private String formatDate(Date date)
 	{
 		SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z");
 		String text = sdf.format(date);
-		return  text.substring(0, text.length() - 2) + ":" + text.substring(text.length() - 2);
+		return text.substring(0, text.length() - 2) + ":" + text.substring(text.length() - 2);
 	}
-	
-	private void addXHTML(Document doc, Element parent, String text, ContentType contentType, URL articleUrl)
-	throws TransformerException, SAXException, IOException
+
+	private void addXHTML(Document doc, Element parent, String text, ContentType contentType, URL articleUrl) throws TransformerException, SAXException,
+			IOException
 	{
 		String content = ContentUtils.formatText(text, articleUrl, contentType, contentFilter);
 		parent.appendChild(doc.createTextNode(content));
