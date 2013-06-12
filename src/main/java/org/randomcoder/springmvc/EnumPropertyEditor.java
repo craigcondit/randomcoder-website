d87441a454976cbb30589a509a0d539f1015147a
Craig Condit <ccondit@randomcoder.org>
2013-06-12 08:11:30 -0700

    Removed embedded license from javadoc; formatting

--- src/main/java/org/randomcoder/springmvc/EnumPropertyEditor.java
+++ src/main/java/org/randomcoder/springmvc/EnumPropertyEditor.java
@@ -6,51 +6,28 @@ import org.apache.commons.logging.*;
 
 /**
  * Enum property editor support.
- * 
- * <pre>
- * Copyright (c) 2006, Craig Condit. All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright notice,
- *     this list of conditions and the following disclaimer in the documentation
- *     and/or other materials provided with the distribution.
- *     
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- * </pre>
  */
+@SuppressWarnings("rawtypes")
 public class EnumPropertyEditor extends PropertyEditorSupport
 {
 	private static final Log logger = LogFactory.getLog(EnumPropertyEditor.class);
 
 	private Class<? extends Enum> enumType;
-	
+
 	/**
 	 * Creates a new property editor.
 	 * 
 	 * @param enumType
-	 *            enum type
+	 *          enum type
 	 */
 	public EnumPropertyEditor(Class<? extends Enum> enumType)
 	{
 		this.enumType = enumType;
 	}
-	
+
 	/**
 	 * Gets the value of the current Enum's name as a {@code String}.
+	 * 
 	 * @return String enum string value
 	 */
 	@Override
@@ -60,15 +37,20 @@ public class EnumPropertyEditor extends PropertyEditorSupport
 
 		Object value = getValue();
 		if (value == null)
+		{
 			return "";
+		}
 		
 		return ((Enum) value).name();
 	}
 
 	/**
 	 * Populates the editor with the Enum object with the given name.
-	 * @param string string value of name property
-	 * @throws IllegalArgumentException if object could not be loaded
+	 * 
+	 * @param string
+	 *          string value of name property
+	 * @throws IllegalArgumentException
+	 *           if object could not be loaded
 	 */
 	@Override
 	@SuppressWarnings("unchecked")
--- src/main/java/org/randomcoder/springmvc/JstlTemplateView.java
+++ src/main/java/org/randomcoder/springmvc/JstlTemplateView.java
@@ -8,31 +8,6 @@ import org.springframework.web.servlet.view.JstlView;
 
 /**
  * Custom JSTL-based template view.
- * 
- * <pre>
- * Copyright (c) 2006, Craig Condit. All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright notice,
- *     this list of conditions and the following disclaimer in the documentation
- *     and/or other materials provided with the distribution.
- *     
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- * </pre>
  */
 public class JstlTemplateView extends JstlView
 {
@@ -45,7 +20,9 @@ public class JstlTemplateView extends JstlView
 
 	/**
 	 * Sets the attributes to add to this template.
-	 * @param attributes map of attribute key/value pairs.
+	 * 
+	 * @param attributes
+	 *          map of attribute key/value pairs.
 	 */
 	public void setAttributes(Map<String, Object> attributes)
 	{
@@ -55,7 +32,9 @@ public class JstlTemplateView extends JstlView
 	/**
 	 * Gets the URL associated with this view.
 	 * 
-	 * <p> Delegates to the parent view if no url was specified. </p>
+	 * <p>
+	 * Delegates to the parent view if no url was specified.
+	 * </p>
 	 * 
 	 * @return url
 	 */
@@ -63,15 +42,21 @@ public class JstlTemplateView extends JstlView
 	public String getUrl()
 	{
 		if (url != null)
+		{
 			return url;
+		}
 		if (parent != null)
+		{
 			return parent.getUrl();
+		}
 		return super.getUrl();
 	}
 
 	/**
 	 * Sets the URL for this view.
-	 * @param url url
+	 * 
+	 * @param url
+	 *          url
 	 */
 	@Override
 	public void setUrl(String url)
@@ -82,7 +67,9 @@ public class JstlTemplateView extends JstlView
 
 	/**
 	 * Sets the name of the request attribute to populate with the template map.
-	 * @param templateName template name (defaults to "template")
+	 * 
+	 * @param templateName
+	 *          template name (defaults to "template")
 	 */
 	public void setTemplateName(String templateName)
 	{
@@ -92,8 +79,12 @@ public class JstlTemplateView extends JstlView
 	/**
 	 * Sets the parent template of this template.
 	 * 
-	 * <p> Several methods in this class delegate to the parent. </p>
-	 * @param parent parent template
+	 * <p>
+	 * Several methods in this class delegate to the parent.
+	 * </p>
+	 * 
+	 * @param parent
+	 *          parent template
 	 */
 	public void setParent(JstlTemplateView parent)
 	{
@@ -103,8 +94,11 @@ public class JstlTemplateView extends JstlView
 	/**
 	 * Adds the template map to the request attribute named by
 	 * {@link #setTemplateName(String)}.
-	 * @param request HTTP request
-	 * @throws Exception if an error occurs
+	 * 
+	 * @param request
+	 *          HTTP request
+	 * @throws Exception
+	 *           if an error occurs
 	 */
 	@Override
 	protected void exposeHelpers(HttpServletRequest request) throws Exception
@@ -120,31 +114,45 @@ public class JstlTemplateView extends JstlView
 	/**
 	 * Populates the given template map with attributes for this template.
 	 * 
-	 * <p> This method merges the template map with that of the parent view if it
-	 * is found. </p>
+	 * <p>
+	 * This method merges the template map with that of the parent view if it is
+	 * found.
+	 * </p>
 	 * 
-	 * @param templateMap map of key / value pairs
+	 * @param templateMap
+	 *          map of key / value pairs
 	 */
 	protected void populateTemplateMap(Map<String, Object> templateMap)
 	{
 		if (parent != null)
+		{
 			parent.populateTemplateMap(templateMap);
+		}
 		if (attributes != null)
+		{
 			templateMap.putAll(attributes);
+		}
 	}
 
 	/**
 	 * Gets the request attribute to store the template map under.
 	 * 
-	 * <p> This method delegates to the parent template if no name is found. </p>
+	 * <p>
+	 * This method delegates to the parent template if no name is found.
+	 * </p>
+	 * 
 	 * @return template name
 	 */
 	protected String getTemplateName()
 	{
 		if (templateName != null)
+		{
 			return templateName;
+		}
 		if (parent != null)
+		{
 			return parent.getTemplateName();
+		}
 		return DEFAULT_TEMPLATE_NAME;
 	}
 
