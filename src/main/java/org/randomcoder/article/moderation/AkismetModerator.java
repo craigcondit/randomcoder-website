62828b0b732d8f7cb038a489edf716a76425a334
Craig Condit <ccondit@randomcoder.org>
2013-06-13 08:05:17 -0700

    Reformatting; removed license from javadoc

--- src/main/java/org/randomcoder/article/moderation/AkismetModerator.java
+++ src/main/java/org/randomcoder/article/moderation/AkismetModerator.java
@@ -14,92 +14,73 @@ import org.springframework.beans.factory.annotation.Required;
 
 /**
  * Moderator implementation which queries Akismet.
- * 
- * <pre>
- * Copyright (c) 2007, Craig Condit. All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright notice,
- *     this list of conditions and the following disclaimer in the documentation
- *     and/or other materials provided with the distribution.
- *     
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- * </pre>
  */
 public class AkismetModerator implements Moderator, InitializingBean, DisposableBean
 {
 	private static final Log logger = LogFactory.getLog(AkismetModerator.class);
-	
+
 	private MultiThreadedHttpConnectionManager connectionManager;
-	
+
 	private String apiKey;
 	private String siteUrl;
 	private String userAgent;
-	
+
 	/**
 	 * Sets the Akismet API key to use.
-	 * @param apiKey API key
+	 * 
+	 * @param apiKey
+	 *          API key
 	 */
 	@Required
 	public void setApiKey(String apiKey)
 	{
 		this.apiKey = apiKey;
 	}
-	
+
 	/**
 	 * Sets the base url of the client site.
-	 * @param siteUrl site url
+	 * 
+	 * @param siteUrl
+	 *          site url
 	 */
 	@Required
 	public void setSiteUrl(String siteUrl)
 	{
 		this.siteUrl = siteUrl;
 	}
-	
+
 	/**
 	 * Sets the application information for this client.
 	 * 
 	 * @param appInfoBusiness
-	 *            application information
+	 *          application information
 	 */
 	@Required
 	public void setAppInfoBusiness(AppInfoBusiness appInfoBusiness)
 	{
 		String appName = appInfoBusiness.getApplicationName();
 		String appVersion = appInfoBusiness.getApplicationVersion();
-		
+
 		appName = appName.replaceAll("\\s+", "-");
 		appVersion = appVersion.replaceAll("\\s+", "_");
-		
-		userAgent = appName + "/" + appVersion + " | " + getClass().getCanonicalName() + "/" + appVersion; 
-		
+
+		userAgent = appName + "/" + appVersion + " | " + getClass().getCanonicalName() + "/" + appVersion;
+
 		logger.info("Using Akismet user agent: " + userAgent);
 	}
-	
+
 	/**
-	 * Initializes the Akismet filter. 
-	 * @throws Exception if an error occurs
+	 * Initializes the Akismet filter.
+	 * 
+	 * @throws Exception
+	 *           if an error occurs
 	 */
 	@Override
 	public void afterPropertiesSet() throws Exception
 	{
 		connectionManager = new MultiThreadedHttpConnectionManager();
 		connectionManager.getParams().setStaleCheckingEnabled(true);
-		
+
 		logger.info("Logging into Akismet...");
 		if (isAkismetAccountValid())
 		{
@@ -108,11 +89,12 @@ public class AkismetModerator implements Moderator, InitializingBean, Disposable
 		else
 		{
 			logger.warn("Akismet login failed... Check api key and site url.");
-		}		
+		}
 	}
-		
+
 	/**
 	 * Gets the Akismet URL to use for account verification.
+	 * 
 	 * @return verification url
 	 */
 	protected String getVerifyKeyUrl()
@@ -122,6 +104,7 @@ public class AkismetModerator implements Moderator, InitializingBean, Disposable
 
 	/**
 	 * Gets the Akismet URL to use for comment checking.
+	 * 
 	 * @return comment check url
 	 */
 	protected String getCommentCheckUrl()
@@ -131,6 +114,7 @@ public class AkismetModerator implements Moderator, InitializingBean, Disposable
 
 	/**
 	 * Gets the Akismet URL to use for spam submission.
+	 * 
 	 * @return submit spam url
 	 */
 	protected String getSubmitSpamUrl()
@@ -140,16 +124,19 @@ public class AkismetModerator implements Moderator, InitializingBean, Disposable
 
 	/**
 	 * Gets the Akismet URL to use for ham submission.
+	 * 
 	 * @return submit ham url
 	 */
 	protected String getSubmitHamUrl()
 	{
 		return "http://" + apiKey + ".rest.akismet.com/1.1/submit-ham";
 	}
-	
+
 	/**
 	 * Shuts down the Akismet filter.
-	 * @throws Exception if an error occurs
+	 * 
+	 * @throws Exception
+	 *           if an error occurs
 	 */
 	@Override
 	public void destroy() throws Exception
@@ -166,15 +153,15 @@ public class AkismetModerator implements Moderator, InitializingBean, Disposable
 			HttpClient client = new HttpClient(connectionManager);
 			post = new PostMethod(getCommentCheckUrl());
 			populatePost(post, comment);
-			int status = client.executeMethod(post); 
+			int status = client.executeMethod(post);
 			if (status != HttpStatus.SC_OK)
 			{
 				throw new ModerationException("Unknown status code from validation service:" + status);
 			}
-			
+
 			String body = post.getResponseBodyAsString().toLowerCase(Locale.US);
-			
-			return "false".equals(body);			
+
+			return "false".equals(body);
 		}
 		catch (IOException e)
 		{
@@ -182,10 +169,16 @@ public class AkismetModerator implements Moderator, InitializingBean, Disposable
 		}
 		finally
 		{
-			if (post != null) try { post.releaseConnection(); } catch (Exception ignored) {}						
+			if (post != null)
+				try
+				{
+					post.releaseConnection();
+				}
+				catch (Exception ignored)
+				{}
 		}
 	}
-		
+
 	@Override
 	public void markAsHam(Comment comment) throws ModerationException
 	{
@@ -195,7 +188,7 @@ public class AkismetModerator implements Moderator, InitializingBean, Disposable
 			HttpClient client = new HttpClient(connectionManager);
 			post = new PostMethod(getSubmitHamUrl());
 			populatePost(post, comment);
-			int status = client.executeMethod(post); 
+			int status = client.executeMethod(post);
 			if (status != HttpStatus.SC_OK)
 			{
 				throw new ModerationException("Unknown status code from validation service:" + status);
@@ -207,7 +200,13 @@ public class AkismetModerator implements Moderator, InitializingBean, Disposable
 		}
 		finally
 		{
-			if (post != null) try { post.releaseConnection(); } catch (Exception ignored) {}						
+			if (post != null)
+				try
+				{
+					post.releaseConnection();
+				}
+				catch (Exception ignored)
+				{}
 		}
 	}
 
@@ -220,7 +219,7 @@ public class AkismetModerator implements Moderator, InitializingBean, Disposable
 			HttpClient client = new HttpClient(connectionManager);
 			post = new PostMethod(getSubmitSpamUrl());
 			populatePost(post, comment);
-			int status = client.executeMethod(post); 
+			int status = client.executeMethod(post);
 			if (status != HttpStatus.SC_OK)
 			{
 				throw new ModerationException("Unknown status code from validation service:" + status);
@@ -232,20 +231,26 @@ public class AkismetModerator implements Moderator, InitializingBean, Disposable
 		}
 		finally
 		{
-			if (post != null) try { post.releaseConnection(); } catch (Exception ignored) {}						
+			if (post != null)
+				try
+				{
+					post.releaseConnection();
+				}
+				catch (Exception ignored)
+				{}
 		}
 	}
 
 	private void populatePost(PostMethod post, Comment comment)
 	{
-		CommentIp ip = comment.getIpAddress();			
+		CommentIp ip = comment.getIpAddress();
 		CommentUserAgent ua = comment.getUserAgent();
 		CommentReferrer ref = comment.getReferrer();
 		User user = comment.getCreatedByUser();
 		String author = (user == null) ? comment.getAnonymousUserName() : user.getUserName();
 		String email = (user == null) ? comment.getAnonymousEmailAddress() : user.getEmailAddress();
 		String url = (user == null) ? comment.getAnonymousWebsite() : user.getWebsite();
-		
+
 		post.setRequestHeader("User-Agent", userAgent);
 		post.addParameter("blog", siteUrl);
 		post.addParameter("user_ip", (ip == null) ? "" : ip.getIpAddress());
@@ -261,25 +266,25 @@ public class AkismetModerator implements Moderator, InitializingBean, Disposable
 			post.addParameter("comment_author_url", url);
 		post.addParameter("comment_content", comment.getContent());
 	}
-	
+
 	private boolean isAkismetAccountValid()
 	{
 		PostMethod post = null;
 		try
 		{
 			HttpClient client = new HttpClient(connectionManager);
-			
+
 			post = new PostMethod(getVerifyKeyUrl());
 			post.setRequestHeader("User-Agent", userAgent);
 			post.addParameter("key", apiKey);
 			post.addParameter("blog", siteUrl);
-			
+
 			if (client.executeMethod(post) != HttpStatus.SC_OK)
 				return false;
-			
+
 			String body = post.getResponseBodyAsString().toLowerCase(Locale.US);
-			
-			return "valid".equals(body);			
+
+			return "valid".equals(body);
 		}
 		catch (Exception e)
 		{
@@ -288,7 +293,13 @@ public class AkismetModerator implements Moderator, InitializingBean, Disposable
 		}
 		finally
 		{
-			if (post != null) try { post.releaseConnection(); } catch (Exception ignored) {}			
+			if (post != null)
+				try
+				{
+					post.releaseConnection();
+				}
+				catch (Exception ignored)
+				{}
 		}
 	}
 }
--- src/main/java/org/randomcoder/article/moderation/Moderator.java
+++ src/main/java/org/randomcoder/article/moderation/Moderator.java
@@ -4,53 +4,37 @@ import org.randomcoder.article.comment.Comment;
 
 /**
  * Interface for generic moderator plugins.
- * 
- * <pre>
- * Copyright (c) 2007, Craig Condit. All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright notice,
- *     this list of conditions and the following disclaimer in the documentation
- *     and/or other materials provided with the distribution.
- *     
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- * </pre>
  */
 public interface Moderator
 {
 	/**
 	 * Validates the given comment as spam or ham.
-	 * @param comment comment to validate
-	 * @throws ModerationException if operation could not be performed
+	 * 
+	 * @param comment
+	 *          comment to validate
+	 * @throws ModerationException
+	 *           if operation could not be performed
 	 * @return true if comment is legitimate, false if comment is spam
 	 */
 	public boolean validate(Comment comment) throws ModerationException;
-	
+
 	/**
 	 * Marks the given comment as spam.
-	 * @param comment comment to mark as spam
-	 * @throws ModerationException if operation could not be performed
+	 * 
+	 * @param comment
+	 *          comment to mark as spam
+	 * @throws ModerationException
+	 *           if operation could not be performed
 	 */
 	public void markAsSpam(Comment comment) throws ModerationException;
-	
+
 	/**
 	 * Marks the given comment as ham (non-spam).
-	 * @param comment comment to mark as ham
-	 * @throws ModerationException if operation could not be performed
+	 * 
+	 * @param comment
+	 *          comment to mark as ham
+	 * @throws ModerationException
+	 *           if operation could not be performed
 	 */
 	public void markAsHam(Comment comment) throws ModerationException;
 }
