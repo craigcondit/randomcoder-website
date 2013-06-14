4ee3dea91a7bba391eda12255ce0e169beb916a0
Craig Condit <ccondit@randomcoder.org>
2013-06-14 09:04:16 -0700

    Moved TagListController functionality into TagController

--- src/main/java/org/randomcoder/mvc/controller/TagController.java
+++ src/main/java/org/randomcoder/mvc/controller/TagController.java
@@ -1,11 +1,15 @@
 package org.randomcoder.mvc.controller;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.apache.commons.logging.*;
 import org.randomcoder.bo.TagBusiness;
 import org.randomcoder.mvc.command.*;
 import org.randomcoder.mvc.validator.*;
+import org.randomcoder.tag.TagStatistics;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,6 +28,8 @@ public class TagController
 	private TagBusiness tagBusiness;
 	private TagAddValidator tagAddValidator;
 	private TagEditValidator tagEditValidator;
+	private int defaultPageSize = 25;
+	private int maximumPageSize = 100;
 
 	/**
 	 * Sets the TagBusiness implementation to use.
@@ -62,6 +68,30 @@ public class TagController
 	}
 
 	/**
+	 * Sets the default number of items to display per page (defaults to 25).
+	 * 
+	 * @param defaultPageSize
+	 *            default number of items per page
+	 */
+	@Value("${tag.pagesize.default}")
+	public void setDefaultPageSize(int defaultPageSize)
+	{
+		this.defaultPageSize = defaultPageSize;
+	}
+
+	/**
+	 * Sets the maximum number of items to allow per page (defaults to 100).
+	 * 
+	 * @param maximumPageSize
+	 *            maximum number of items per page
+	 */
+	@Value("${tag.pagesize.max}")
+	public void setMaximumPageSize(int maximumPageSize)
+	{
+		this.maximumPageSize = maximumPageSize;
+	}
+	
+	/**
 	 * Binds validators.
 	 * 
 	 * @param binder
@@ -81,6 +111,43 @@ public class TagController
 		}
 	}
 
+	/**
+	 * Generates the tag list.
+	 * 
+	 * @param cmd
+	 *            form command
+	 * @param model
+	 *            MVC model
+	 * @return tag list view
+	 */
+	@RequestMapping("/tag")
+	public String tagList(TagListCommand cmd, Model model)
+	{
+		// set range
+		int start = Math.max(cmd.getStart(), 0);
+		cmd.setStart(start);
+
+		int limit = cmd.getLimit();
+		if (limit <= 0)
+		{
+			limit = defaultPageSize;
+		}
+		limit = Math.min(limit,  maximumPageSize);
+		
+		cmd.setLimit(limit);
+
+		List<TagStatistics> tagStats = tagBusiness.queryTagStatisticsInRange(start, limit);
+		int count = tagBusiness.countTags();
+
+		// populate model
+		model.addAttribute("tagStats", tagStats);
+		model.addAttribute("pageCount", count);
+		model.addAttribute("pageStart", start);
+		model.addAttribute("pageLimit", limit);
+
+		return "tag-list";
+	}
+	
 	/**
 	 * Begins adding a tag.
 	 * 
