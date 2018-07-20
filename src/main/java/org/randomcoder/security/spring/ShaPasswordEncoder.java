006a59c08ce0c37c81be7c58e03f77023e22c2d8
Craig Condit <ccondit@randomcoder.org>
2018-07-20 15:20:34 -0500

    Fix password encoder

--- src/main/java/org/randomcoder/security/spring/ShaPasswordEncoder.java
+++ src/main/java/org/randomcoder/security/spring/ShaPasswordEncoder.java
@@ -1,9 +1,30 @@
 package org.randomcoder.security.spring;
 
-public class ShaPasswordEncoder {
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-	public ShaPasswordEncoder() {
-		// TODO Auto-generated constructor stub
+import org.springframework.security.crypto.codec.Hex;
+import org.springframework.security.crypto.codec.Utf8;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class ShaPasswordEncoder implements PasswordEncoder {
+
+	private MessageDigest digester() {
+		try {
+			return MessageDigest.getInstance("SHA-1");
+		} catch (NoSuchAlgorithmException e) {
+			throw new IllegalArgumentException("Unable to get SHA-1 digest");
+		}
+	}
+
+	@Override
+	public String encode(CharSequence rawPassword) {
+		return new String(Hex.encode(digester().digest(Utf8.encode(rawPassword))));
+	}
+
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		return encode(rawPassword).equals(encodedPassword);
 	}
 
 }
